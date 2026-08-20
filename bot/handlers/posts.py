0ec1f3640418ba0930ada25fff5e9e2910a28f38
Andrey Ilyin <jemmix@gmail.com>
2026-08-20 17:48:19 +0200

    fix: minimize diff vs master in posts.py and import_posts_to_dev.py

--- bot/handlers/posts.py
+++ bot/handlers/posts.py
@@ -50,11 +50,13 @@ async def unsubscribe(update: Update, context: CallbackContext) -> None:
         post=post,
     )
 
-    if is_unsubscribed:
-        await update.callback_query.answer(
-            text=f"Вы отписались от о комментариев к посту «{post.title}» 🔕"
-        )
-    else:
-        await update.callback_query.answer(
-            text="Вы и не были подписаны на уведомления к этому посту ❌"
-        )
+    if user.telegram_id:
+
+        if is_unsubscribed:
+            await update.callback_query.answer(
+                text=f"Вы отписались от о комментариев к посту «{post.title}» 🔕"
+            )
+        else:
+            await update.callback_query.answer(
+                text="Вы и не были подписаны на уведомления к этому посту ❌"
+            )
